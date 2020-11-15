068f38174cf90aaad1c8e33d323db1c8d1cbc705
Mostofa Patwary <mostofa.patwary@gmail.com>
2020-11-14 23:35:25 -0800

    Updated tokentype

--- tasks/squad/t5/finetune.py
+++ tasks/squad/t5/finetune.py
@@ -45,7 +45,7 @@ def question_answering(Dataset, name_from_datapath_func):
 
         print_rank_0('building T5 model for {} ...'.format(args.task))
 
-        return T5Model(num_tokentypes=2,
+        return T5Model(num_tokentypes=0,
                        parallel_output=False)
 
     def single_dataset_provider(datapath, name=None):
