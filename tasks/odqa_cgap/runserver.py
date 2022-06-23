243e4215ec662b1c11fb8dcfaa2d417183094cc3
dasu <dasu@nvidia.com>
2022-06-22 22:55:20 -0700

    set debug mode to False

--- tasks/odqa_cgap/runserver.py
+++ tasks/odqa_cgap/runserver.py
@@ -5,4 +5,4 @@ sys.path.insert(0, "../App")
 from App import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='10.33.1.207', port=8084)
+    app.run(debug=False, host='10.33.1.207', port=8084)
