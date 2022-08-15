dedf6ca83d6858801507de6fb5ae8d052ef54ca0
Lawrence McAfee <lmcafee@nvidia.com>
2022-08-15 12:39:40 -0700

    index verification working.

--- retrieval/build/build_index.py
+++ retrieval/build/build_index.py
@@ -118,18 +118,15 @@ def verify_index(args, timer):
     base_index_path = index.add(args.add_paths, args.index_dir_path, timer)
     timer.pop()
 
-    raise Exception("add base.")
-
     timer.push("add-test")
     test_index_path = run_add_pipeline(args, timer)
     timer.pop()
-
-    raise Exception("add test.")
 
     torch.distributed.barrier()
 
     if torch.distributed.get_rank() == 0:
 
+        # Read indexes.
         timer.push("read")
         base_index = faiss.read_index(base_index_path)
         test_index = faiss.read_index(test_index_path)
@@ -137,24 +134,49 @@ def verify_index(args, timer):
         test_invlists = faiss.extract_index_ivf(test_index).invlists
         timer.pop()
 
+        # # ###########################
+        # # Test #1: Serialize indexes.
+        # # ###########################
+
+        # base_np = faiss.serialize_index(base_index)
+        # test_np = faiss.serialize_index(test_index)
+        # # base_invlist_np = faiss.serialize_index(base_invlists)
+        # # test_invlist_np = faiss.serialize_index(test_invlists)
+
+        # pax({
+        #     "base_np" : str(base_np),
+        #     "test_np" : str(test_np),
+        #     "base_np / hash" : hash(base_np.tobytes()),
+        #     "test_np / hash" : hash(test_np.tobytes()),
+        #     # "base_invlist_np" : str(base_invlist_np),
+        #     # "test_invlist_np" : str(test_invlist_np),
+        #     # "base_invlist_np / hash" : hash(base_invlist_np.tobytes()),
+        #     # "test_invlist_np / hash" : hash(test_invlist_np.tobytes()),
+        #     "equal" : np.array_equal(base_np, test_np),
+        # })
+
+        ############################################
+        # Test #2: Compare each list's ids/codes.
+        # - Note: only relevant if above test fails.
+        ############################################
+
+        # Verify same list size.
         assert base_index.ntotal == test_index.ntotal
 
+        # Compare each list's ids/codes.
         timer.push("add")
         for list_id in range(args.ncluster):
 
-            if list_id % 1000 == 0:
+            if list_id % 100000 == 0:
                 print("verify list %d / %d." % (list_id, args.ncluster))
 
-            # pax({"args": args})
-
+            # Get list size, ids, codes.
             base_list_size = base_invlists.list_size(list_id)
             base_ids = np.empty((base_list_size,), dtype = "i8")
             base_codes = np.empty((base_list_size, args.pq_m), dtype = "uint8")
             test_list_size = test_invlists.list_size(list_id)
             test_ids = np.empty((test_list_size,), dtype = "i8")
             test_codes = np.empty((test_list_size, args.pq_m), dtype = "uint8")
-
-            assert base_list_size == test_list_size
 
             base_id_ptr = base_invlists.get_ids(list_id)
             base_code_ptr = base_invlists.get_codes(list_id)
@@ -169,6 +191,8 @@ def verify_index(args, timer):
             test_invlists.release_ids(list_id, test_id_ptr)
             test_invlists.release_codes(list_id, test_code_ptr)
 
+            # Verify list size, ids, codes.
+            assert base_list_size == test_list_size
             assert np.array_equal(base_ids, test_ids)
             assert np.array_equal(base_codes, test_codes)
 
@@ -185,15 +209,15 @@ def verify_index(args, timer):
 
         timer.pop()
 
-        pax({
-            "base_invlists" : base_invlists,
-            "test_invlists" : test_invlists,
-        })
+        # pax({
+        #     "base_invlists" : base_invlists,
+        #     "test_invlists" : test_invlists,
+        # })
 
+    # Final sync. [ unnecessary ]
     torch.distributed.barrier()
 
     print_seq([ base_index_path, test_index_path ])
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if __name__ == "__main__":
