22a4991deeb3264c3c06565225f10e5752a32682
Lawrence McAfee <lmcafee@nvidia.com>
2022-09-27 14:19:55 -0700

    parallelizing chunk index build.

--- tools/retrieval/preprocess/index_chunks.py
+++ tools/retrieval/preprocess/index_chunks.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import concurrent.futures
 from functools import reduce
 # import glob
 import h5py
@@ -21,6 +22,7 @@ import json
 import numpy as np
 import os
 from pathlib import Path
+import threading
 # import time
 import torch
 from tqdm import tqdm
@@ -80,6 +82,7 @@ def get_sorted_dataset_metadatas(args, workdir):
 
     return data_metas
 
+
 def save_dataset_metadatas(workdir, data_metas):
 
     # Save dataset order.
@@ -87,23 +90,214 @@ def save_dataset_metadatas(workdir, data_metas):
     with open(order_path, "w") as f:
         json.dump(data_metas, f, indent = 4) # remove 'indent', once debugged.
 
-def build_individual_chunk_index(args, indexed_dataset):
 
-    gpt_tokenizer = _GPT2BPETokenizer(
-        vocab_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/gpt3/bpe/gpt2-vocab.json",
-        merge_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/gpt3/bpe/gpt2-merges.txt",
-    )
-    bert_tokenizer = _BertWordPieceTokenizer(
-        vocab_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/roberta_mmap/vocab.txt",
-        lower_case = True,
-    )
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# def build_partial_chunk_index(
+#         # thread_id,
+#         indexed_dataset,
+#         document_id,
+#         chunk_id,
+#         n_chunks,
+#         chunk_start_idx,
+#         chunk_end_idx,
+#         gpt_tokenizer,
+#         bert_tokenizer,
+# ):
 
-    size = indexed_dataset.sizes.shape[0]
-    train = int(round(float(size) * 0.98))
+#     # thread_id = threading.get_ident()
 
-    # eods = []
+#     # if chunk_id % 10 == 0:
+#     #     print("processing doc %d, chunk %d / %d." % (document_id, chunk_id, n_chunks))
+
+#     gpt_token_ids = indexed_dataset.get(
+#         idx = document_id,
+#         offset = chunk_start_idx,
+#         length = chunk_end_idx - chunk_start_idx,
+#     )
+#     gpt_token_ids = [t for t in gpt_token_ids.tolist() # unnecessary?
+#                      if t != gpt_tokenizer.eod]
+#     text = gpt_tokenizer.detokenize(gpt_token_ids)
+#     bert_token_ids = bert_tokenizer.tokenize(text)
+
+#     # if len(bert_token_ids) > 0:
+#     #     chunk_index.append((
+#     #         document_id,
+#     #         chunk_start_idx,
+#     #         chunk_end_idx,
+#     #         len(bert_token_ids),
+#     #     ))
+#     # >>>
+#     # if len(bert_token_ids) == 0:
+#     #     pax({
+#     #         "gpt_tokenizer" : gpt_tokenizer,
+#     #         "bert_tokenizer" : bert_tokenizer,
+#     #         "text" : text,
+#     #         "gpt_token_ids" : "%d / %s" % (
+#     #             len(gpt_token_ids),
+#     #             str(gpt_token_ids),
+#     #         ),
+#     #         "bert_token_ids" : "%d / %s" % (
+#     #             len(bert_token_ids),
+#     #             str(bert_token_ids),
+#     #         ),
+#     #     })
+#     # <<<
+
+#     return len(bert_token_ids)
+#     # return text, gpt_token_ids, bert_token_ids
+
+
+# def build_individual_chunk_index(args, indexed_dataset):
+
+#     gpt_tokenizer = _GPT2BPETokenizer(
+#         vocab_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/gpt3/bpe/gpt2-vocab.json",
+#         merge_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/gpt3/bpe/gpt2-merges.txt",
+#     )
+#     bert_tokenizer = _BertWordPieceTokenizer(
+#         vocab_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/roberta_mmap/vocab.txt",
+#         lower_case = True,
+#     )
+
+#     # size = indexed_dataset.sizes.shape[0]
+#     # train = int(round(float(size) * 0.98))
+
+#     # eods = []
+#     chunk_index = []
+
+#     for document_id, document in enumerate(tqdm(indexed_dataset)):
+
+#         # >>>
+#         # if document_id == 1000:
+#         #     break
+#         # <<<
+
+#         if document_id == train:
+#             break
+
+#         eod = document[-1]
+#         document = document[:-1]
+#         document_len = len(document)
+
+#         chunk_start_idxs = list(range(0, document_len, args.retrieval_chunk_len))
+#         chunk_end_idxs = [min(document_len, s + args.retrieval_chunk_len)
+#                           for s in chunk_start_idxs]
+
+#         # eods.append(eod)
+#         # >>>
+#         # chunk_index.extend([(document_id, *idxs)
+#         #                     for idxs in zip(chunk_start_idxs, chunk_end_idxs)])
+#         # +++
+#         n_threads = 8
+#         # with concurrent.futures.ThreadPoolExecutor(max_workers = n_threads) as executor:
+#         with concurrent.futures.ProcessPoolExecutor(max_workers = n_threads) as executor:
+#             futures = []
+#             for i, chunk_start_idx in enumerate(chunk_start_idxs):
+#                 futures.append(executor.submit(
+#                     build_partial_chunk_index,
+#                     # thread_id = i % n_threads, # not real thread id
+#                     indexed_dataset,
+#                     document_id,
+#                     i,
+#                     len(chunk_start_idxs),
+#                     chunk_start_idx,
+#                     chunk_end_idxs[i],
+#                     gpt_tokenizer,
+#                     bert_tokenizer,
+#                 ))
+#             bert_chunk_lens = []
+#             for future in concurrent.futures.as_completed(futures):
+#                 bert_chunk_lens.append(future.result())
+
+#             # thread_start_idxs = list(range(0, len(chunk_start_idxs), n_threads))
+#         # pax({"bert_chunk_lens": bert_chunk_lens})
+
+#         chunk_index.extend([(document_id, *idxs) for idxs in zip(
+#             chunk_start_idxs,
+#             chunk_end_idxs,
+#             bert_chunk_lens,
+#         )])
+
+#         # pax({
+#         #     "chunk_start_idxs / len" : len(chunk_start_idxs),
+#         #     "thread_start_idxs" : str(thread_start_idxs),
+#         #     "n_threads" : n_threads,
+#         # })
+#         # chunk_ranges = []
+#         # for i, chunk_start_idx in enumerate(chunk_start_idxs):
+#         #     chunk_end_idx = chunk_end_idxs[i]
+#         #     gpt_token_ids = indexed_dataset.get(
+#         #         idx = document_id,
+#         #         offset = chunk_start_idx,
+#         #         length = chunk_end_idx - chunk_start_idx,
+#         #     )
+#         #     gpt_token_ids = [t for t in gpt_token_ids.tolist() # unnecessary?
+#         #                      if t != gpt_tokenizer.eod]
+#         #     text = gpt_tokenizer.detokenize(gpt_token_ids)
+#         #     bert_token_ids = bert_tokenizer.tokenize(text)
+
+#         #     if True or len(bert_token_ids) > 0:
+#         #         chunk_index.append((
+#         #             document_id,
+#         #             chunk_start_idx,
+#         #             chunk_end_idx,
+#         #             len(bert_token_ids),
+#         #         ))
+#         #     # >>>
+#         #     else:
+#         #         pax({
+#         #             "gpt_tokenizer" : gpt_tokenizer,
+#         #             "bert_tokenizer" : bert_tokenizer,
+#         #             "text" : text,
+#         #             "gpt_token_ids" : "%d / %s" % (
+#         #                 len(gpt_token_ids),
+#         #                 str(gpt_token_ids),
+#         #             ),
+#         #             "bert_token_ids" : "%d / %s" % (
+#         #                 len(bert_token_ids),
+#         #                 str(bert_token_ids),
+#         #             ),
+#         #         })
+#         #     # <<<
+
+#     pax({"chunk_index / len": len(chunk_index)})
+
+#     print(' > converting chunk index to numpy.')
+#     # eods = np.array(eods)
+#     chunk_index = np.array(chunk_index)
+
+#     # return eods, chunk_index
+#     return chunk_index
+def build_partial_chunk_index(
+        proc_id,
+        n_procs,
+        indexed_dataset,
+        gpt_tokenizer,
+        bert_tokenizer,
+):
+
+    # n_documents = len(indexed_dataset)
+    n_documents = len(indexed_dataset.doc_idx) - 1 # doc_idx starts at 0
+    n_documents_per_proc = int(np.ceil(n_documents / n_procs))
+    document_start_id = proc_id * n_documents_per_proc
+    document_end_id = min(n_documents, document_start_id + n_documents_per_proc)
+
+    # pax({
+    #     # "indexed_dataset" : indexed_dataset,
+    #     "n_procs" : n_procs,
+    #     "n_documents" : n_documents,
+    #     "n_documents_per_proc" : n_documents_per_proc,
+    #     "doc coverage" : n_procs * n_documents_per_proc,
+    # })
+    print(" > building partial chunk index, proc %d / %d, docs %d:%d / %d." % (
+        proc_id,
+        n_procs,
+        document_start_id,
+        document_end_id,
+    ))
+
+    return proc_id
+
     chunk_index = []
-
     for document_id, document in enumerate(tqdm(indexed_dataset)):
 
         # >>>
@@ -123,45 +317,137 @@ def build_individual_chunk_index(args, indexed_dataset):
                           for s in chunk_start_idxs]
 
         # eods.append(eod)
+        # >>>
         # chunk_index.extend([(document_id, *idxs)
         #                     for idxs in zip(chunk_start_idxs, chunk_end_idxs)])
-        # >>>
-        chunk_ranges = []
-        for i, chunk_start_idx in enumerate(chunk_start_idxs):
-            chunk_end_idx = chunk_end_idxs[i]
-            gpt_token_ids = indexed_dataset.get(
-                idx = document_id,
-                offset = chunk_start_idx,
-                length = chunk_end_idx - chunk_start_idx,
-            )
-            gpt_token_ids = [t for t in gpt_token_ids.tolist() # unnecessary?
-                             if t != gpt_tokenizer.eod]
-            text = gpt_tokenizer.detokenize(gpt_token_ids)
-            bert_token_ids = bert_tokenizer.tokenize(text)
+        # +++
 
-            if True or len(bert_token_ids) > 0:
-                chunk_index.append((
-                    document_id,
-                    chunk_start_idx,
-                    chunk_end_idx,
-                    len(bert_token_ids),
-                ))
-            # >>>
-            else:
-                pax({
-                    "gpt_tokenizer" : gpt_tokenizer,
-                    "bert_tokenizer" : bert_tokenizer,
-                    "text" : text,
-                    "gpt_token_ids" : "%d / %s" % (
-                        len(gpt_token_ids),
-                        str(gpt_token_ids),
-                    ),
-                    "bert_token_ids" : "%d / %s" % (
-                        len(bert_token_ids),
-                        str(bert_token_ids),
-                    ),
-                })
-            # <<<
+            # thread_start_idxs = list(range(0, len(chunk_start_idxs), n_threads))
+        # pax({"bert_chunk_lens": bert_chunk_lens})
+
+        chunk_index.extend([(document_id, *idxs) for idxs in zip(
+            chunk_start_idxs,
+            chunk_end_idxs,
+            bert_chunk_lens,
+        )])
+
+        # pax({
+        #     "chunk_start_idxs / len" : len(chunk_start_idxs),
+        #     "thread_start_idxs" : str(thread_start_idxs),
+        #     "n_threads" : n_threads,
+        # })
+        # chunk_ranges = []
+        # for i, chunk_start_idx in enumerate(chunk_start_idxs):
+        #     chunk_end_idx = chunk_end_idxs[i]
+        #     gpt_token_ids = indexed_dataset.get(
+        #         idx = document_id,
+        #         offset = chunk_start_idx,
+        #         length = chunk_end_idx - chunk_start_idx,
+        #     )
+        #     gpt_token_ids = [t for t in gpt_token_ids.tolist() # unnecessary?
+        #                      if t != gpt_tokenizer.eod]
+        #     text = gpt_tokenizer.detokenize(gpt_token_ids)
+        #     bert_token_ids = bert_tokenizer.tokenize(text)
+
+        #     if True or len(bert_token_ids) > 0:
+        #         chunk_index.append((
+        #             document_id,
+        #             chunk_start_idx,
+        #             chunk_end_idx,
+        #             len(bert_token_ids),
+        #         ))
+        #     # >>>
+        #     else:
+        #         pax({
+        #             "gpt_tokenizer" : gpt_tokenizer,
+        #             "bert_tokenizer" : bert_tokenizer,
+        #             "text" : text,
+        #             "gpt_token_ids" : "%d / %s" % (
+        #                 len(gpt_token_ids),
+        #                 str(gpt_token_ids),
+        #             ),
+        #             "bert_token_ids" : "%d / %s" % (
+        #                 len(bert_token_ids),
+        #                 str(bert_token_ids),
+        #             ),
+        #         })
+        #     # <<<
+
+    # thread_id = threading.get_ident()
+
+    # if chunk_id % 10 == 0:
+    #     print("processing doc %d, chunk %d / %d." % (document_id, chunk_id, n_chunks))
+
+    gpt_token_ids = indexed_dataset.get(
+        idx = document_id,
+        offset = chunk_start_idx,
+        length = chunk_end_idx - chunk_start_idx,
+    )
+    gpt_token_ids = [t for t in gpt_token_ids.tolist() # unnecessary?
+                     if t != gpt_tokenizer.eod]
+    text = gpt_tokenizer.detokenize(gpt_token_ids)
+    bert_token_ids = bert_tokenizer.tokenize(text)
+
+    # if len(bert_token_ids) > 0:
+    #     chunk_index.append((
+    #         document_id,
+    #         chunk_start_idx,
+    #         chunk_end_idx,
+    #         len(bert_token_ids),
+    #     ))
+    # >>>
+    # if len(bert_token_ids) == 0:
+    #     pax({
+    #         "gpt_tokenizer" : gpt_tokenizer,
+    #         "bert_tokenizer" : bert_tokenizer,
+    #         "text" : text,
+    #         "gpt_token_ids" : "%d / %s" % (
+    #             len(gpt_token_ids),
+    #             str(gpt_token_ids),
+    #         ),
+    #         "bert_token_ids" : "%d / %s" % (
+    #             len(bert_token_ids),
+    #             str(bert_token_ids),
+    #         ),
+    #     })
+    # <<<
+
+    return len(bert_token_ids)
+    # return text, gpt_token_ids, bert_token_ids
+
+
+def build_individual_chunk_index(args, indexed_dataset):
+
+    gpt_tokenizer = _GPT2BPETokenizer(
+        vocab_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/gpt3/bpe/gpt2-vocab.json",
+        merge_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/gpt3/bpe/gpt2-merges.txt",
+    )
+    bert_tokenizer = _BertWordPieceTokenizer(
+        vocab_file = "/gpfs/fs1/projects/gpu_adlr/datasets/nlp/roberta_mmap/vocab.txt",
+        lower_case = True,
+    )
+
+    n_procs = 8
+
+    # size = indexed_dataset.sizes.shape[0]
+    # train = int(round(float(size) * 0.98))
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers = n_threads) as executor:
+        futures = []
+        for proc_id in range(n_procs): # not true process id
+            futures.append(executor.submit(
+                build_partial_chunk_index,
+                proc_id,
+                n_procs,
+                indexed_dataset,
+                gpt_tokenizer,
+                bert_tokenizer,
+            ))
+        partial_chunk_indexes = []
+        for future in concurrent.futures.as_completed(futures):
+            partial_chunk_indexes.append(future.result())
+
+    pax({"partial_chunk_indexes": partial_chunk_indexes})
 
     pax({"chunk_index / len": len(chunk_index)})
 
@@ -171,6 +457,7 @@ def build_individual_chunk_index(args, indexed_dataset):
 
     # return eods, chunk_index
     return chunk_index
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 def build_individual_chunk_indexes(args, workdir, data_metas):
 
