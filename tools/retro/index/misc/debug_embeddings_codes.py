041551bb3aef484de0846d6878c0312976d16764
Lawrence McAfee <lmcafee@nvidia.com>
2023-01-11 11:07:25 -0800

    updated license.

--- tools/retro/index/misc/debug_embeddings_codes.py
+++ tools/retro/index/misc/debug_embeddings_codes.py
@@ -1,19 +1,9 @@
-# coding=utf-8
-# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
 
 import glob
+import h5py
+import numpy as np
+import torch
 
 from megatron import get_retro_args, print_rank_0
 from tools.bert_embedding import BertEmbedder, DiskDataParallelBertEmbedder
@@ -28,118 +18,6 @@ from lutil import pax
 # <<<
 
 
-# >>> [ fake add! ]
-# def add(self, text_dataset):
-
-#     # pax(0, {"n_threads" : faiss.omp_get_max_threads()})
-
-#     if torch.distributed.get_rank() != 0:
-#         return
-
-#     import glob
-
-#     added_index_path = self.get_added_index_path()
-#     if os.path.exists(added_index_path):
-#         raise Exception("index already written.")
-
-#     embedding_paths = sorted(glob.glob("/gpfs/fs1/projects/gpu_adlr/datasets/lmcafee/retro/workdirs/wiki/index/faiss-par-add/IVF262144_HNSW32,Flat/train_tmp/*.hdf5"))
-#     code_paths = sorted(glob.glob("/gpfs/fs1/projects/gpu_adlr/datasets/lmcafee/retro/workdirs/wiki/index/faiss-par-add/IVF262144_HNSW32,Flat/add-v0/add_tmp/*.hdf5"))
-
-#     assert len(embedding_paths) == len(code_paths)
-
-#     # pax(0, {
-#     #     "embedding_paths" : embedding_paths,
-#     #     "code_paths" : code_paths,
-#     # })
-
-#     from tools.bert_embedding import BertEmbedder
-#     from tools.retro.db.utils import get_merged_valid_dataset
-#     from tools.retro.utils import GPTToTextDataset
-#     args = get_retro_args()
-#     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-#     print("embedder.")
-#     # embedder = BertEmbedder(
-#     #     args.retro_bert_batch_size,
-#     #     args.retro_bert_max_chunk_length,
-#     #     "megatron",
-#     # )
-#     embedder = BertEmbedder(args.retro_bert_batch_size,
-#                             args.retro_bert_max_chunk_length,
-#                             args.bert_embedder_type)
-#     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-#     print("valid dataset.")
-#     valid_dataset = GPTToTextDataset(get_merged_valid_dataset())
-#     print("valid text.")
-#     valid_text = valid_dataset[0]["text"]
-#     print("valid embed.")
-#     valid_embed = embedder.embed_text(valid_text).reshape((1, -1))
-
-#     # pax({
-#     #     "valid_text" : valid_text,
-#     #     "valid_embed" : valid_embed,
-#     # })
-
-#     index0 = self.get_empty_index()
-#     index1 = faiss.clone_index(index0)
-
-#     for index in (index0, index1):
-#         faiss.ParameterSpace().set_index_parameter(index, "efSearch",
-#                                                    args.retro_ef_search)
-#         faiss.ParameterSpace().set_index_parameter(index, "nprobe",
-#                                                    args.retro_nprobe)
-
-
-#     # Add embeddings.
-#     print("add embeddings.")
-#     # for path_idx, embedding_path in enumerate(tqdm(embedding_paths)):
-#     # for path_idx in tqdm(range(66, len(embedding_paths))):
-#     for path_idx in tqdm(range(len(embedding_paths))):
-
-#         with h5py.File(embedding_paths[path_idx]) as f:
-#             embeddings0 = np.copy(f["data"])
-#             index0.add(embeddings0)
-#         with h5py.File(code_paths[path_idx]) as f:
-#             codes1 = np.copy(f["data"])
-#             index1.add_sa_codes(codes1)
-
-#         bal0 = index0.invlists.imbalance_factor()
-#         bal1 = index1.invlists.imbalance_factor()
-
-#         nbrs0 = index0.search(valid_embed, 5)
-#         nbrs1 = index1.search(valid_embed, 5)
-
-#         if True or bal0 != bal1 or not np.array_equal(nbrs0, nbrs1):
-#             codes0 = index0.sa_encode(embeddings0)
-#             codes0_path = "/gpfs/fs1/projects/gpu_adlr/datasets/lmcafee/retro/workdirs/wiki/index/faiss-par-add/IVF262144_HNSW32,Flat/scratch/codes0.hdf5"
-#             with h5py.File(codes0_path, "w") as f:
-#                 f.create_dataset("data", data = codes0)
-#             with h5py.File(codes0_path) as f:
-#                 codes0b = np.copy(f["data"])
-#             pax(0, {
-#                 # "n_threads" : faiss.omp_get_max_threads(),
-#                 "path_idx" : path_idx,
-#                 "index0 / ntotal" : index0.ntotal,
-#                 "index1 / ntotal" : index1.ntotal,
-#                 "bal0" : bal0,
-#                 "bal1" : bal1,
-#                 "nbrs0" : nbrs0,
-#                 "nbrs1" : nbrs1,
-#                 "--" : "--",
-#                 "embeddings0" : str(embeddings0.flatten().tolist()),
-#                 "codes0" : str(codes0.flatten().tolist()),
-#                 "codes0b" : str(codes0b.flatten().tolist()),
-#                 "codes1" : str(codes1.flatten().tolist()),
-#                 "codes0 == codes1" : np.array_equal(codes0, codes1),
-#                 "codes0 == codes0b" : np.array_equal(codes0, codes0b),
-#             })
-
-#         # break
-
-#     # Write index.
-#     print_rank_0("write added index.")
-#     faiss.write_index(index0, added_index_path)
-
-#     raise Exception("index written.")
 def add(self, text_dataset):
 
     if torch.distributed.get_rank() != 0:
@@ -269,11 +147,6 @@ def embed_db_mockup():
                                             args.retro_block_size,
                                             args.bert_embedder_type)
     embedder.embed_text_dataset("index", train_data_dir, text_dataset)
-
-import glob
-import h5py
-import numpy as np
-import torch
 
 def print_training_embeddings():
 
--- tools/retro/pretraining/retro_dataset.py
+++ tools/retro/pretraining/retro_dataset.py
@@ -1,28 +1,13 @@
-# coding=utf-8
-# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
 
-# from collections import defaultdict
-# import glob
-# import h5py
-# import numpy as np
-# import os
+import h5py
+import numpy as np
+import os
 import torch
 
-# from megatron import get_args, get_retro_args
-# from tools.retro.db.utils import get_merged_train_dataset as get_db_dataset
-# from tools.retro.pretraining.chunk_dataset import get_chunk_dataset_map
+from megatron import get_args, get_retro_args
+from tools.retro.db.utils import get_merged_train_dataset as get_db_dataset
+from tools.retro.pretraining.chunk_dataset import get_chunk_dataset_map
 
 
 class RetroDataset(torch.utils.data.Dataset):
