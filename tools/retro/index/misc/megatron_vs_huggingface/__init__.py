6970a46b69a7b90c2a2a73f7590606a6aca324ed
Lawrence McAfee <lmcafee@nvidia.com>
2023-01-18 11:30:31 -0800

    removed misc dirs/files.

--- tools/retro/index/misc/megatron_vs_huggingface/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
-
-from .full_db import compare_bert_full_db
-from .partial_db import compare_bert_partial_db
-from .neighbor_dists import compare_bert_neighbor_dists
