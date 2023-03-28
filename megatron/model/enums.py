dbc08d4059e36bc5ed9b91f062d02d373d31c9e3
Lawrence McAfee <lmcafee@nvidia.com>
2023-03-28 13:52:15 -0700

    cleaned enums.

--- megatron/model/enums.py
+++ megatron/model/enums.py
@@ -5,19 +5,15 @@ import enum
 class ModelType(enum.Enum):
     encoder_or_decoder = 1
     encoder_and_decoder = 2
-    # >>>
     retro_encoder = 3
     retro_decoder = 4
-    # <<<
 
 class LayerType(enum.Enum):
     encoder = 1
     decoder = 2
-    # >>>
     retro_encoder = 3
     retro_decoder = 4
     retro_decoder_with_retriever = 5
-    # <<<
  
 class AttnType(enum.Enum):
     self_attn = 1
