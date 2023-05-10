81fa68f2ccc8a7829b213aee27ab98ff7e2e0e83
Peng Xu <pengx@nvidia.com>
2023-05-10 23:53:20 +0000

    change node numbers

--- examples/foundational_qa/common_args.sh
+++ examples/foundational_qa/common_args.sh
@@ -24,7 +24,7 @@ if [[ $model_size == "43b" ]]; then
     hid_dim=8192
     heads=64
     pip_par=4
-    if [[ $model_card == *-pp1 ]]; then
+    if [[ $model_card == *pp1 ]]; then
         pip_par=1
     fi
 fi
