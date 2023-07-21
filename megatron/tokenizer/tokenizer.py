562fb2a2d21c4071b29436342e00d04ab06c13c5
Lawrence McAfee <lmcafee@nvidia.com>
2023-07-21 21:53:53 +0000

    added llama2 tokenizer.

--- megatron/tokenizer/tokenizer.py
+++ megatron/tokenizer/tokenizer.py
@@ -8,7 +8,6 @@ from abc import abstractmethod
 from .bert_tokenization import FullTokenizer as FullBertTokenizer
 from .gpt2_tokenization import GPT2Tokenizer
 
-
 def build_tokenizer(args):
     """Initialize tokenizer."""
     if args.rank == 0:
@@ -36,6 +35,13 @@ def build_tokenizer(args):
     elif args.tokenizer_type == 'GPTSentencePieceTokenizer':
         assert args.tokenizer_model is not None
         tokenizer = _GPTSentencePieceTokenizer(args.tokenizer_model)
+    elif args.tokenizer_type == 'Llama2':
+        assert args.tokenizer_model is not None
+        tokenizer = _Llama2Tokenizer(args.tokenizer_model)
+        # >>>
+        from lutil import pax
+        pax({"tokenizer": tokenizer})
+        # <<<
     elif args.tokenizer_type == 'NullTokenizer':
         assert args.vocab_size is not None
         tokenizer = _NullTokenizer(args.vocab_size)
@@ -481,6 +487,107 @@ class _GPTSentencePieceTokenizer(_SentencePieceTokenizer):
 
     def detokenize(self, ids):
         return self.tokenizer.decode_ids(ids)
+
+    @property
+    def cls(self):
+        return -1
+
+    @property
+    def sep(self):
+        return -1
+
+    @property
+    def mask(self):
+        return -1
+
+    @property
+    def eod(self):
+        return self._eos_id
+
+    @property
+    def additional_special_tokens_ids(self):
+        return None
+
+# >>>
+# import os
+# from logging import getLogger
+# from typing import List
+
+# from sentencepiece import SentencePieceProcessor
+
+# logger = getLogger()
+# <<<
+
+# class Llama2Tokenizer(AbstractTokenizer):
+# class Llama2Tokenizer(AbstractTokenizer):
+
+#     def __init__(self, model_path: str):
+
+#         super().__init__()
+
+#         # reload tokenizer
+#         assert os.path.isfile(model_path), model_path
+#         self.sp_model = SentencePieceProcessor(model_file=model_path)
+#         logger.info(f"Reloaded SentencePiece model from {model_path}")
+
+#         # BOS / EOS token IDs
+#         self.n_words: int = self.sp_model.vocab_size()
+#         self.bos_id: int = self.sp_model.bos_id()
+#         self.eos_id: int = self.sp_model.eos_id()
+#         self.pad_id: int = self.sp_model.pad_id()
+#         logger.info(
+#             f"#words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id}"
+#         )
+#         assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
+
+#         # >>>
+#         from lutil import pax
+#         pax({"model_path": model_path, "vocab_size": self.vocab_size})
+#         # <<<
+
+#     # @property
+#     # def vocab_size(self):
+#     #     return self.sp_model.vocab_size()
+
+#     def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
+#         assert type(s) is str
+#         t = self.sp_model.encode(s)
+#         if bos:
+#             t = [self.bos_id] + t
+#         if eos:
+#             t = t + [self.eos_id]
+#         return t
+
+#     def decode(self, t: List[int]) -> str:
+#         return self.sp_model.decode(t)
+class _Llama2Tokenizer(_SentencePieceTokenizer):
+    """SentencePieceTokenizer-Megatron wrapper"""
+
+    def __init__(self, model_file,):
+        super().__init__(model_file, vocab_extra_ids=0)
+
+    def _initalize(self, vocab_extra_ids):
+        self._populate_vocab()
+
+        # BOS / EOS token IDs
+        self.n_words: int = self.tokenizer.vocab_size()
+        self.bos_id: int = self.tokenizer.bos_id()
+        self.eos_id: int = self.tokenizer.eos_id()
+        self.pad_id: int = self.tokenizer.pad_id()
+        assert self.tokenizer.vocab_size() == self.tokenizer.get_piece_size()
+
+    def tokenize(self, s: str, bos=True, eos=False):
+        '''Default args for text completion, not chat/dialog.'''
+        assert type(s) is str
+        t = self.tokenizer.encode(s)
+        if bos:
+            t = [self.bos_id] + t
+        if eos:
+            t = t + [self.eos_id]
+        return t
+
+    def detokenize(self, ids):
+        return self.tokenizer.decode(t)
 
     @property
     def cls(self):
