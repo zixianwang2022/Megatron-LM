978e066fcc7638c4666e86d9c52487a70f99449d
chankyulee <chankyu@nvidia.com>
2023-07-26 20:16:51 +0000

    sft checkpoint location add v4(marigold_badger) and v5(fqa_v1)

--- examples/qa/config.sh
+++ examples/qa/config.sh
@@ -1,4 +1,4 @@
-DATA_HOME="../../inform-retriever"
+DATA_HOME="../../inform-retriever-msmarco"
 
 sample_input_file="${DATA_HOME}/data/$TASK/${split}.json"
 DATA_FOLDER="${DATA_HOME}/data/$TASK"
@@ -62,9 +62,16 @@ if [[ ${model_card} == *-pp1-v2 ]]; then
     ## unbias_cuckoo
     PRETRAINED_CHECKPOINT="/lustre/fsw/portfolios/llmservice/users/chankyul/megatron_qa_2/mega/megatron_converted_chat_oasst_43B_tp8"
 fi
-
 if [[ ${model_card} == *-pp1-v3 ]]; then
     ## quiet_cockatoo
     PRETRAINED_CHECKPOINT="/lustre/fs1/portfolios/adlr/projects/adlr_nlp_llmnext/megatron_sft_quiet_cockatoo_tp8_pp1"
+fi
+if [[ ${model_card} == *-pp1-v4 ]]; then
+    ## marigold_badger
+    PRETRAINED_CHECKPOINT="/lustre/fsw/portfolios/llmservice/users/chankyul/megatron_qa_2/marigold_badger_mega"
+fi
+if [[ ${model_card} == *-pp1-v5 ]]; then
+    ## fqa_v1
+    PRETRAINED_CHECKPOINT="/lustre/fs1/portfolios/adlr/users/pengx/qa_blendv12_gpt_1e-8_conv_quiet_cockatoo_pp1_same_format_ctx1_43b_64_3e-7"
 fi
 TOKENIZER_MODEL="/lustre/fsw/portfolios/adlr/projects/adlr_nlp_llmnext/mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model"
