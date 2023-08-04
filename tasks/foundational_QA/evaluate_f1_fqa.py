bc59d15669daadedcee2d7605405fb045d3c2c45
peng xu <pengx@nvidia.com>
2023-08-03 18:10:36 -0700

    fix inference bug for v2

--- tasks/foundational_QA/evaluate_f1_fqa.py
+++ tasks/foundational_QA/evaluate_f1_fqa.py
@@ -29,6 +29,7 @@ def compute_f1_score(predicted_answers, groundtruth_answer, exp_name="default"):
     precision, recall, f1 = F1Metric.compute_all_pairs(guess_list, answer_list)
     print('Method: %s; Precision: %.4f; recall: %.4f; f1: %.4f' % (\
         exp_name, precision, recall, f1))
+    return f1
 
 
 def load_groundtruth_file(data_file):
@@ -67,62 +68,70 @@ def evaluate_f1(ground_truth_file, prediction_file, reduced_test_only=False):
     groundtruth_answer = load_groundtruth_file(ground_truth_file)
     predicted_answers = load_prediction(prediction_file)
     if not reduced_test_only:
-        compute_f1_score(predicted_answers, groundtruth_answer)
-    groundtruth_answer, predicted_answers = groundtruth_answer[:43], predicted_answers[:43]
-    compute_f1_score(predicted_answers, groundtruth_answer)
+        return compute_f1_score(predicted_answers, groundtruth_answer)
+    # groundtruth_answer, predicted_answers = groundtruth_answer[:43], predicted_answers[:43]
+    # compute_f1_score(predicted_answers, groundtruth_answer)
         
 if __name__ == "__main__":
 
+        import argparse
+        argparser = argparse.ArgumentParser(description="run fqa eval")
+        argparser.add_argument("--step", type=int, default=4500)
+        args = argparser.parse_args()
+        all_f1 = []
         model_name = "qa_blendv0_gpt_1e-8_conv_quiet_cockatoo_pp1_same_format_ctx1_43b_64_3e-7"
         model_name = "qa_blendv12_gpt_1e-8_conv_quiet_cockatoo_pp1_fixed_newsqa_same_format_ctx1_43b_64_3e-7"
         model_name = "qa_blendv13_gpt_1e-8_conv_quiet_cockatoo_pp1_same_format_ctx1_43b_64_3e-7"
         model_name = "qa_blendv12_gpt_1e-8_conv_quiet_cockatoo_pp1_fixed_doc2dial_same_format_ctx1_43b_64_3e-7"
         ckpt_path="/lustre/fsw/adlr/adlr-nlp/pengx/sft_43b_qa/checkpoints/applications/{}/".format(model_name)
         n_ctx=5
-        prediction_file = ckpt_path + "/inference_input_retriever_dragon_msmarcominilm_doc2dial_{}_generate_43b_test_greedy_0_250_4500_ret.txt.v2".format(n_ctx)
+        step=args.step
+        prediction_file = ckpt_path + "/inference_input_retriever_dragon_msmarcominilm_doc2dial_{}_generate_43b_test_greedy_0_250_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/retro/data/inference_input_retriever_dragon_msmarcominilm_doc2dial/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
 
-        prediction_file = ckpt_path + "/Iternal_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved_{}_generate_43b_test_greedy_0_250_4500_ret.txt.v2".format(n_ctx)
+        prediction_file = ckpt_path + "/Iternal_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved_{}_generate_43b_test_greedy_0_250_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/retro/data/Iternal_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
 
-        prediction_file = ckpt_path + "/NVIT_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved_{}_generate_43b_test_greedy_0_250_4500_ret.txt.v2".format(n_ctx)
+        prediction_file = ckpt_path + "/NVIT_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved_{}_generate_43b_test_greedy_0_250_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/retro/data/NVIT_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
 
-        prediction_file = ckpt_path + "/nv_benefits_dragon_retriever300_retrieved_generic_{}_generate_43b_test_greedy_0_250_4500_ret.txt.v2".format(n_ctx)
+        prediction_file = ckpt_path + "/nv_benefits_dragon_retriever300_retrieved_generic_{}_generate_43b_test_greedy_0_250_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/retro/data/nv_benefits_dragon_retriever300_retrieved_generic/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
 
-        prediction_file = ckpt_path + "/landrover_plus_benz_clean_plus_ford_tasb_ftmsmarcominilm_chunkbysents150_benzlandroverford_retrieved_{}_generate_43b_test_greedy_0_250_4500_ret.txt.v2".format(n_ctx)
+        prediction_file = ckpt_path + "/landrover_plus_benz_clean_plus_ford_tasb_ftmsmarcominilm_chunkbysents150_benzlandroverford_retrieved_{}_generate_43b_test_greedy_0_250_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/retro/data/landrover_plus_benz_clean_plus_ford_tasb_ftmsmarcominilm_chunkbysents150_benzlandroverford_retrieved/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
 
-        prediction_file = ckpt_path + "/ford_tasb_ftmsmarcominilm_chunkbysents150_benzlandroverford_retrieved_{}_generate_43b_test_greedy_0_250_4500_ret.txt.v2".format(n_ctx)
+        prediction_file = ckpt_path + "/ford_tasb_ftmsmarcominilm_chunkbysents150_benzlandroverford_retrieved_{}_generate_43b_test_greedy_0_250_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/retro/data/ford_tasb_ftmsmarcominilm_chunkbysents150_benzlandroverford_retrieved/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
 
-        prediction_file = ckpt_path + "/att_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved_{}_generate_43b_test_greedy_0_250_4500_ret.txt.v2".format(n_ctx)
+        prediction_file = ckpt_path + "/att_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved_{}_generate_43b_test_greedy_0_250_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/data/att/att_dragon_retriever_msmarcominilm_reranker_chunkbysents300_retrieved/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
 
-        prediction_file = ckpt_path + "/nq_{}_generate_43b_test_greedy_0_200_4500_ret.txt.v2".format(n_ctx)
+        prediction_file = ckpt_path + "/nq_{}_generate_43b_test_greedy_0_200_{}_ret.txt.v2".format(n_ctx, step)
         ground_truth_file = "/lustre/fsw/adlr/adlr-nlp/pengx/retro/data/NQ/test.json"
         print(prediction_file)
         print(ground_truth_file)
-        evaluate_f1(ground_truth_file, prediction_file)
+        all_f1.append(evaluate_f1(ground_truth_file, prediction_file))
+
+        print("average f1", sum(all_f1) / len(all_f1))
