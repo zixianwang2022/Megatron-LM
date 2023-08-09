9ec09598355fc4fcd07ccdc4df3584f29d9adb18
Lawrence McAfee <lmcafee@nvidia.com>
2023-08-09 11:18:12 -0700

    selene updates.

--- scripts/args_gen.sh
+++ scripts/args_gen.sh
@@ -51,24 +51,22 @@ else
 fi
 
 # >>>
-MEGATRON_REPO_DIR="/home/lmcafee/src/megatrons/megatron-lm-llama2-loader"
-LLAMA_REPO_DIR="/lustre/fsw/portfolios/adlr/users/lmcafee/llama/2/llama"
-TOKENIZER_PATH="/lustre/fsw/portfolios/adlr/users/lmcafee/llama/2/llama/tokenizer.model"
+# MEGATRON_REPO_DIR="/home/lmcafee/src/megatrons/megatron-lm-llama2-loader"
+# LLAMA_REPO_DIR="/lustre/fsw/portfolios/adlr/users/lmcafee/llama/2/llama"
+# TOKENIZER_PATH="/lustre/fsw/portfolios/adlr/users/lmcafee/llama/2/llama/tokenizer.model"
 
-# MODEL_SIZE="7b"
-# MODEL_SIZE="13b"
-# MODEL_SIZE="70b"
-
-COMMON_CHECKPOINT_DIR="/lustre/fsw/portfolios/adlr/users/lmcafee/llama/2/llama/checkpoints"
-MEGATRON_CHECKPOINT_DIR="${COMMON_CHECKPOINT_DIR}/megatron/${MODEL_SIZE}"
-LLAMA_CHECKPOINT_DIR="${COMMON_CHECKPOINT_DIR}/llama/llama-2-${MODEL_SIZE}"
+# COMMON_CHECKPOINT_DIR="/lustre/fsw/portfolios/adlr/users/lmcafee/llama/2/llama/checkpoints"
+# MEGATRON_CHECKPOINT_DIR="${COMMON_CHECKPOINT_DIR}/megatron/text/${MODEL_SIZE}"
+# LLAMA_CHECKPOINT_DIR="${COMMON_CHECKPOINT_DIR}/llama/text/llama-2-${MODEL_SIZE}"
 # +++
-# MEGATRON_REPO_DIR="/lustre/fsw/adlr/adlr-nlp/lmcafee/src/megatrons/megatron-lm-llama2-loader"
-# LLAMA_REPO_DIR="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/llama"
-# # TOKENIZER_PATH="/lustre/fsw/adlr/adlr-nlp/adlr-nlp-sharing/nvllm-1.1t/utils/mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model"
-# TOKENIZER_PATH="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/llama/tokenizer_hf/tokenizer.model"
-# MEGATRON_CHECKPOINT_DIR="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/llama/llama-2-7b-megatron"
-# LLAMA_CHECKPOINT_DIR="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/llama/llama-2-7b"
+MEGATRON_REPO_DIR="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/megatron/megatron-lm-llama2-loader"
+LLAMA_REPO_DIR="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/llama"
+# TOKENIZER_PATH="/lustre/fsw/adlr/adlr-nlp/adlr-nlp-sharing/nvllm-1.1t/utils/mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model"
+TOKENIZER_PATH="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/llama/tokenizer_hf/tokenizer.model"
+
+COMMON_CHECKPOINT_DIR="/lustre/fsw/adlr/adlr-nlp/lmcafee/data/llama/2/checkpoints"
+MEGATRON_CHECKPOINT_DIR="${COMMON_CHECKPOINT_DIR}/megatron/text/${MODEL_SIZE}"
+LLAMA_CHECKPOINT_DIR="${COMMON_CHECKPOINT_DIR}/llama/text/llama-2-${MODEL_SIZE}"
 # <<<
 
 ######## args. ########
