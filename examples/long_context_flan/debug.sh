8b46a245401c8985808af6b2fec1bf9583af6ffc
peng xu <pengx@nvidia.com>
2023-09-02 16:28:01 -0700

    add debug.sh

---
+++ examples/long_context_flan/debug.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# bash examples/qa/finetune_normal_lm.sh landrover_tasb_retrieved 843m 1 3e-6 1 
+
+blend_name=$1
+model_size=$2
+global_bsz=$3
+lr=$4
+model_card=$5
+
+train_iters=43000
+
+. ./examples/long_context_flan/long_context_args.sh
+
+num_nodes=1
+num_gpus=8
+
+min_lr=0.000001
+if [[ $model_size == "8b" ]]; then
+    num_nodes=4
+    min_lr=0.00000001
+fi
+
+if [[ $model_size == "43b" ]]; then
+    num_nodes=64
+    min_lr=0.00000001
+fi
+
+SAVENAME="${blend_name}_${model_card}_${model_size}_${global_bsz}_${lr}"
+CHECKPOINT_PATH="${QA_HOME}/checkpoints/applications/${SAVENAME}"
+TENSORBOARD_DIR="${QA_HOME}/tensorboard/${SAVENAME}"
+mkdir -p ${TENSORBOARD_DIR}
+
+OUTPUT_ARGS="--log-interval 1 \
+             --save-interval 1500 \
+             --eval-interval 4 \
+             --tensorboard-dir ${TENSORBOARD_DIR} \
+             --eval-iters 2"
+
+. ./examples/foundational_qa/${blend_name}.sh
+
+options=" \
+    $GPT_ARGS \
+    --data-path ${DATA_BLEND} \
+    --data-folder ${data_folder} \
+    --sequence-parallel \
+    --lr $lr \
+    --micro-batch-size 1 \
+    --global-batch-size 2 \
+    --min-lr ${min_lr} \
+    --train-iters ${train_iters} \
+    --dataloader-type cyclic \
+    --save $CHECKPOINT_PATH \
+    $OUTPUT_ARGS \
+    $FT_ARGS"
+
+if [[ -f "$CHECKPOINT_PATH/latest_checkpointed_iteration.txt" ]]; then
+    options="$options \
+        --load $CHECKPOINT_PATH "
+else
+    options="$options \
+        --load $PRETRAINED_CHECKPOINT \
+        --finetune \
+	--no-load-rng \
+        --no-load-optim "
+fi
+
+DIR=`pwd`
+# -m torch.distributed.launch --nproc_per_node 8
+run_cmd="python -u ${DIR}/tasks/foundational_QA/finetune_gpt_with_pretrain.py ${options}"
+# srun -l \
+#      --container-image "gitlab-master.nvidia.com/adlr/megatron-lm/boxinw/faissgpu" \
+#      --container-mounts "/home/pengx/projects/retro/:/home/pengx/projects/retro/" \
+#      --output=$DIR/logs/%x_%j_$DATETIME.log sh -c "${run_cmd}"
+# $run_cmd
+
+export SUBMIT_LOGS="${QA_HOME}/megatron-lm/logs"
+mkdir -p $SUBMIT_LOGS
+export NCCL_DEBUG=INFO
+
+export NCCL_IB_TIMEOUT=19
+export NCCL_IB_SL=1
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+
+echo ${run_cmd}
+submit_job --gpu ${num_gpus} --nodes 4 --email_mode never  --mounts $MOUNTS --partition $PARTITION  --image $DOCKER -c "$LAUNCH ${run_cmd}" -n "${SAVENAME}" --duration 0.5  --exclude luna-0534,luna-0253,luna-0377 # --dependent_clones 1
