4f67b48a07aa9f0ca92f741f4f17874fb480ec96
peng xu <pengx@nvidia.com>
2023-09-06 00:55:38 -0700

    update bash to support more ckpt and ct_qc

--- examples/long_context_flan/run_script.sh
+++ examples/long_context_flan/run_script.sh
@@ -8,6 +8,7 @@ bash examples/long_context_flan/finetune_normal_lm.sh qc 43b  64 3e-7 gpt3-43b-m
 bash examples/long_context_flan/finetune_qc_sandeep.sh gpt3-43b-multi-1.1t-gtc-itp-16k-tian
 bash examples/long_context_flan/finetune_qc_sandeep.sh gpt3-43b-multi-1.1t-gtc-itp-32k-tian-tp16pp4
 bash examples/long_context_flan/finetune_qc_sandeep.sh gpt3-43b-multi-1.1t-gtc-base
+bash examples/long_context_flan/ct_qc_sandeep.sh qc_gpt3-43b-multi-1.1t-gtc-base_43b_128_5e-6_continue_2k
 
 bash examples/long_context_flan/debug.sh flanv1 43b  64 3e-7 gpt3-43b-multi-1.1t-gtc-itp-16k-tian
 bash examples/long_context_flan/debug.sh flanv1 43b  64 3e-7 gpt3-43b-multi-1.1t-gtc-base
