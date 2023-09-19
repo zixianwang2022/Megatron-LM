6ea18831fb75217785f59c1098824270d8494716
peng xu <pengx@nvidia.com>
2023-09-19 14:18:45 -0700

    update bash

--- examples/long_context_flan_llama2/run_gen_blends.sh
+++ examples/long_context_flan_llama2/run_gen_blends.sh
@@ -1,8 +1,8 @@
 # model_name=qc_llama2_text_70b_base_70b_128_5e-6
-# model_name=qc_llama2_text_70b_itp-16k_70b_128_1.0e-5_70b_128_5e-6
-# step=1000
-model_name=qc_llama2_text_70b_base_70b_64_2e-5
-step=4000
+model_name=qc_llama2_text_70b_itp-16k_70b_128_1.0e-5_70b_128_5e-6
+step=1000
+# model_name=qc_llama2_text_70b_base_70b_64_2e-5
+# step=4000
 
 num_ctxs=5
 # echo `ls /lustre/fsw/adlr/adlr-nlp/pengx/inform-retriever/code/scroll_eval_data`
@@ -13,25 +13,25 @@ num_ctxs=5
 #     echo bash examples/long_context_flan/generate_multijob_ckpt_step_cross_sft.sh $task 70b greedy test 0 200 $step $num_ctxs $model_name
 # done
 
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh gov_report.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh gov_report.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh gov_report.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh gov_report.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
 bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh narrative_qa.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name true
 bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh narrative_qa.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qasper.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qasper.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qmsum.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qmsum.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh quality.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh quality.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh summ_screen_fd.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh summ_screen_fd.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
-
-
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh musique.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh musique.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh hotpotqa.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh hotpotqa.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh multifieldqa_en.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh multifieldqa_en.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
-bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh doc2dial_full_dialogue.dragon_retriever_chunkbysents300 70b greedy test 0 1000 $step $num_ctxs $model_name true
-ash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh doc2dial_full_dialogue.dragon_retriever_chunkbysents300 70b greedy test 0 1000 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qasper.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qasper.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qmsum.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh qmsum.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh quality.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh quality.dragon_retriever_chunkbysents300 70b greedy test 0 2000 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh summ_screen_fd.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh summ_screen_fd.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
+# 
+# 
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh musique.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh musique.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh hotpotqa.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh hotpotqa.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh multifieldqa_en.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh multifieldqa_en.dragon_retriever_chunkbysents300 70b greedy test 0 200 $step $num_ctxs $model_name
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh doc2dial_full_dialogue.dragon_retriever_chunkbysents300 70b greedy test 0 1000 $step $num_ctxs $model_name true
+# bash examples/long_context_flan_llama2/generate_multijob_ckpt_step_cross_sft.sh doc2dial_full_dialogue.dragon_retriever_chunkbysents300 70b greedy test 0 1000 $step $num_ctxs $model_name
