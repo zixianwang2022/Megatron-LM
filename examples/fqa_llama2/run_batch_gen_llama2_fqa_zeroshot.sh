e4c09a803f9bc997232ad8021ec357db1224410e
zihan_liu <zihanl@nvidia.com>
2023-09-21 11:07:28 -0700

    update run_batch_gen_llama2_fqa_zeroshot.sh

--- examples/fqa_llama2/run_batch_gen_llama2_fqa_zeroshot.sh
+++ examples/fqa_llama2/run_batch_gen_llama2_fqa_zeroshot.sh
@@ -1,5 +1,7 @@
 
-model_name=multiturn_qa_blend_commercial_v5_llama2_text_70b_with_qc_multiturn_same_format_ctx1_70b_64_3e-7
+# model_name=multiturn_qa_blend_commercial_v5_llama2_text_70b_with_qc_multiturn_same_format_ctx1_70b_64_3e-7
+# model_name=multiturn_qa_blendv2_llama2_text_70b_with_qc_multiturn_same_format_ctx1_70b_64_5e-6
+model_name=multiturn_qa_blendv2_llama2_text_70b_with_qc_multiturn_same_format_ctx1_70b_64_1e-6
 num_ctxs=5
 
 ## single-turn qa (batch-1)
@@ -20,12 +22,12 @@ bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh multirc 70b greedy test
 bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh race 70b greedy test 0 1000 3000 $num_ctxs $model_name true
 bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh TextbookQA 70b greedy test 0 1000 3000 $num_ctxs $model_name true
 
-# ## multi-turn qa
-# # doc2dial 3939 samples
-# bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh doc2dial 70b greedy test 0 1000 3000 $num_ctxs $model_name true
-# # quac 7354 samples
-# bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh quac 70b greedy test 0 1000 3000 $num_ctxs $model_name true
-# # qrecc 2805 samples
-# bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh qrecc 70b greedy test 0 1000 3000 $num_ctxs $model_name true
-# # sharc 10000 samples
-# bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh sharc 70b greedy test 0 1000 3000 $num_ctxs $model_name true
+## multi-turn qa
+# doc2dial 3939 samples
+bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh doc2dial 70b greedy test 0 1000 3000 $num_ctxs $model_name true
+# quac 7354 samples
+bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh quac 70b greedy test 0 1000 3000 $num_ctxs $model_name true
+# qrecc 2805 samples
+bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh qrecc 70b greedy test 0 1000 3000 $num_ctxs $model_name true
+# sharc 10000 samples
+bash examples/fqa_llama2/generate_llama2_fqa_zeroshot.sh sharc 70b greedy test 0 1000 3000 $num_ctxs $model_name true
