601a74d696dd837fc549162d3c07a0d91914c0fd
Jon Barker <jbarker@draco-oci-login-01.cm.cluster>
2023-10-26 09:07:35 -0700

    Updated OCI container in interactive script

--- cluster_scripts/interactive_oci.sh
+++ cluster_scripts/interactive_oci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 srun -p interactive,batch_singlenode -A llmservice_nlp_fm -N 1 --pty \
-     --container-image /lustre/fsw/adlr/adlr-nlp/jbarker/checkpoints/adlr+megatron-lm+pytorch+23.04-py3-jbarker.sqsh \
+     --container-image /lustre/fsw/portfolios/llmservice/users/jbarker/workspace/containers/adlr+megatron-lm+pytorch+23.04-py3-jbarker.sqsh \
      --container-mounts "/lustre" \
      --job-name "llmservice_nlp_fm-megatron-dev:interactive" \
      -t 2:00:00 \
