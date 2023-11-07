429d2eda4b403099c73545d2b705e70ab03b3760
Jon Barker <jbarker@selene-login-02.nvidia.com>
2023-11-07 09:50:17 -0800

    Updated hybrid-sam-clip branch to latest CI tests

---
+++ tests/functional_tests/test_scripts/flamingocaptioning/interactive_flamingocaptioning_2b_CLIP336_test.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# These paths should be modified by user to use their source code and output dir
+CHECKPOINT_PATH=/lustre/fsw/adlr/adlr-nlp/jbarker/next-llm/output/flamingocaptioning_2b_CLIP336_test/checkpoints
+TENSORBOARD_DIR=/lustre/fsw/adlr/adlr-nlp/jbarker/next-llm/output/flamingocaptioning_2b_CLIP336_test/tensorboard_logs
+SCRIPTS_DIR=/lustre/fsw/adlr/adlr-nlp/jbarker/next-llm/source/megatron-lm-flamingo-tests
+
+DATA_PATH=/lustre/fsw/adlr/adlr-nlp/adlr_ci/megatron/data/flamingo_data
+mkdir /workspace/data
+ln -s $DATA_PATH /workspace/data/flamingo_data
+
+USE_CORE=0
+USE_TE=0
+MBS=4
+GBS=32
+MAX_STEPS=50
+NUM_NODES=1
+TP_SIZE=1
+PP_SIZE=1
+export ADDITIONAL_PARAMS="--load /lustre/fsw/adlr/adlr-nlp/adlr_ci/megatron/data/flamingo_data/gpt3-2b-multi-1.1t-gtc --visual-arch L_14 --visual-type vit --visual-path /lustre/fsw/adlr/adlr-nlp/adlr_ci/megatron/data/flamingo_data/vit_L_14_336px --img-h 336 --img-w 336"
+
+bash ./tests/functional_tests/test_scripts/flamingocaptioning/finetune_flamingocaptioning_distributed_test.sh DATA_PATH=$DATA_PATH CHECKPOINT_PATH=$CHECKPOINT_PATH TENSORBOARD_DIR=$TENSORBOARD_DIR SCRIPTS_DIR=$SCRIPTS_DIR USE_TE=$USE_TE TP_SIZE=$TP_SIZE PP_SIZE=$PP_SIZE VP_SIZE=$VP_SIZE NUM_NODES=$NUM_NODES MAX_STEPS=$MAX_STEPS USE_CORE=$USE_CORE MBS=$MBS GBS=$GBS
+
+python tests/functional_tests/python_test_utils/get_test_results_from_tensorboard_logs.py /lustre/fsw/adlr/adlr-nlp/jbarker/next-llm/output/flamingocaptioning_2b_CLIP336_test/tensorboard_logs flamingocaptioning_2b_CLIP336_test
+
+export EXPECTED_METRICS_FILE=tests/functional_tests/test_results/flamingocaptioning/flamingocaptioning_2b_CLIP336_test.json
+
+export LOGS_DIR=/lustre/fsw/adlr/adlr-nlp/jbarker/next-llm/output/flamingocaptioning_2b_CLIP336_test/tensorboard_logs
+
+pytest tests/functional_tests/python_test_utils/test_ci_pipeline.py
---
+++ tests/functional_tests/test_scripts/flamingoocr/finetune_flamingoocr_distributed_test.sh
@@ -0,0 +1,123 @@
+#! /bin/bash
+echo "------ARGUMENTS LIST --------"
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+   echo "$KEY=$VALUE"
+done
+echo "---------------------------------"
+
+set -x
+if [[ -n $MBS ]]; then MBS=1; fi
+if [[ -n $GBS ]]; then GBS=8; fi
+
+GPUS_PER_NODE=8
+# Change for multinode config
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE*$NUM_NODES))
+command="export CUDA_DEVICE_MAX_CONNECTIONS=1;"
+
+TRANSFORMER_IMPL=local
+TRAINING_DTYPE=bf16
+
+if [[ $USE_CORE -eq 1 ]]; then
+       echo "Running using megatron core"
+       TRANSFORMER_IMPL=local
+       TRAINING_DTYPE=bf16
+       command="$command export NVTE_ALLOW_NONDETERMINISTIC_ALGO=0;"
+       USE_MCORE=1
+       export NVTE_ALLOW_NONDETERMINISTIC_ALGO=0
+fi
+
+if [[ $USE_TE -eq 1 ]]; then
+       echo "Running with TransformerEngine ..."
+       TRANSFORMER_IMPL=transformer_engine
+       TRAINING_DTYPE=bf16
+else
+       echo "Running with local transformer implementation ..."
+fi
+set +x
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NUM_NODES"
+
+torch_run_cmd="torchrun $DISTRIBUTED_ARGS \
+    pretrain_flamingo.py \
+    --use-flash-attn \
+    --apply-layernorm-1p \
+    --untie-embeddings-and-output-weights \
+    --disable-bias-linear \
+    --no-position-embedding \
+    --use-rotary-position-embeddings \
+    --rotary-percent 0.5 \
+    --swiglu \
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0 \
+    --tensor-model-parallel-size $TP_SIZE \
+    --pipeline-model-parallel-size $PP_SIZE \
+    --num-layers 24 \
+    --hidden-size 2048 \
+    --num-attention-heads 16 \
+    --seq-length 96 \
+    --ds-seq-length 512 \
+    --max-position-embeddings 4096 \
+    --micro-batch-size ${MBS:-4} \
+    --global-batch-size ${GBS:-32} \
+    --cyclic-train-iters $MAX_STEPS \
+    --train-iters $MAX_STEPS \
+    --timing-log-level 2 \
+    --lr 1e-4 \
+    --min-lr 5e-5 \
+    --lr-decay-style cosine \
+    --log-interval 1 \
+    --eval-iters 10 \
+    --eval-interval 1000 \
+    --save-interval 50 \
+    --save $CHECKPOINT_PATH \
+    --tokenizer-type GPTSentencePieceTokenizer \
+    --tokenizer-model ${DATA_PATH}/mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model \
+    --data-path ${DATA_PATH}/ocr_ft.yaml \
+    --valid-path ${DATA_PATH}/ocr_ft.yaml \
+    --prompt-path GPT4-prompts.json \
+    --dset-config dataset.yaml \
+    --split 100,0,0 \
+    --clip-grad 1.0 \
+    --weight-decay 0.1 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.95 \
+    --init-method-std 0.014 \
+    --add-gated-xattn \
+    --add-BOS \
+    --log-params-norm \
+    --log-num-zeros-in-grad \
+    --log-validation-ppl-to-tensorboard \
+    --log-timers-to-tensorboard \
+    --${TRAINING_DTYPE} \
+    --DDP-impl local \
+    --no-load-optim \
+    --eod-mask-loss \
+    --finetune \
+    --perceiver-type none \
+    --freeze-LM \
+    --freeze-ViT \
+    --dataloader-type cyclic --no-data-sharding \
+    --align-to-old \
+    --transformer-impl $TRANSFORMER_IMPL \
+    --dataset-type nvgpt4 \
+    ${USE_MCORE:+--use-mcore-models} \
+    --tensorboard-dir ${TENSORBOARD_DIR} \
+    ${ADDITIONAL_PARAMS:+$ADDITIONAL_PARAMS}"
+
+command="$command $torch_run_cmd"
+echo "-------------------- THE FINAL FINETUNE SCRIPT COMMAND THAT WILL BE RUN ------------"
+echo "$command"
+echo "------------------------------------------------------------------------------"
+
+echo "$command" > $SCRIPTS_DIR/finetune_flamingo_distributed_test.sh
+eval $command
