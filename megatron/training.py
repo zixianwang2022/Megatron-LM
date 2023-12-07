b84bc95e32fa5e84e7baf1527c1340278f9949a9
Jon Barker <jbarker@nvidia.com>
2023-12-07 02:59:18 -0800

    Fixed bugs with normalization layers

--- megatron/training.py
+++ megatron/training.py
@@ -888,11 +888,11 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
         args.curr_iteration = iteration
         loss_dict, skipped_iter, grad_norm, num_zeros_in_grad = \
             train_step(forward_step_func,
-                       train_data_iterator,
-                       model,
-                       optimizer,
-                       opt_param_scheduler,
-                       config, visual_model=visual_model)
+                    train_data_iterator,
+                    model,
+                    optimizer,
+                    opt_param_scheduler,
+                    config, visual_model=visual_model)
         iteration += 1
         args.consumed_train_samples += mpu.get_data_parallel_world_size() * \
                                        args.micro_batch_size * \
