aa17d99e36e7a5b846d5ad6ae142a32d640d5ae3
Peng Xu <pengx@nvidia.com>
2023-12-21 02:14:12 -0800

    add long_fqa_research.sh

---
+++ examples/long_context_flan_llama2/long_fqa_research.sh
@@ -0,0 +1,14 @@
+DATA_BLEND="0.055 drop \
+0.076 NarrativeQA \
+0.02 Quoref \
+0.02 ROPES \
+0.076 squad1.1 \
+0.076 squad2.0 \
+0.076 newsqa \
+0.08 convqa \
+0.16 chatgptgennoanswer \
+0.022 qmsum \
+0.045 qasper \
+0.088 narrative_qa \
+0.044 quality \
+0.16 quiet_cockatoo"
