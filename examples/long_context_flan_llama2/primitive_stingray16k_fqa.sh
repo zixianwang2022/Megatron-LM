5a2d4a7342606fa67f0dc59e86bb27431a76065f
Peng Xu <pengx@nvidia.com>
2023-12-21 03:04:42 -0800

    add bash

---
+++ examples/long_context_flan_llama2/primitive_stingray16k_fqa.sh
@@ -0,0 +1,10 @@
+DATA_BLEND="0.055 drop \
+0.076 NarrativeQA \
+0.02 Quoref \
+0.02 ROPES \
+0.076 squad1.1 \
+0.076 squad2.0 \
+0.076 newsqa \
+0.08 convqa \
+0.16 chatgptgennoanswer \
+0.36 primitive-stingray16k"
