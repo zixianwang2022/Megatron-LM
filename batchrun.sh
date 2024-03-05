bf2377419daf3f0f299c5f20773f3df8c10d66c8
Ethan He <yihuih@nvidia.com>
2024-03-05 08:27:51 -0800

    batchrun

---
+++ batchrun.sh
@@ -0,0 +1,10 @@
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_it10_aux_initexp.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_it10_z_aux_initexp.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_it10_z_initexp_router001_exp001_warmup.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_it10_z_initexp_router001_exp001.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_it10_z_initexp_router001_warmup.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_it10_z_initexp_router001.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_it10_z_initexp.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/te_2b_8_lr1e-5_z_initexp.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/upcycling8x15b_warmup400k_it1_initexp_router001_aux.sh
+sbatch /home/yihuih/llmservice/moe-mlm/examples/moe/upcycling8x15b_warmup400k_it1_initexp_router001.sh
