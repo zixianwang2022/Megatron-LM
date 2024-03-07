5b2c133387c463b504e593850441a024d7e0f58f
Lawrence McAfee <lmcafee@nvidia.com>
2024-03-07 13:09:14 -0800

    remove root imports, for unit tests to pass.

--- megatron/core/models/retro/data/__init__.py
+++ megatron/core/models/retro/data/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
 
-from .config import RetroGPTChunkDatasets
-from .query.multi_split_gpt_dataset import MultiSplitGPTDataset, MultiSplitGPTDatasetConfig
-from .query.retro_dataset import get_retro_datasets
+# ** following imports cause unit tests to fail. **
+
+# from .config import RetroGPTChunkDatasets
+# from .query.multi_split_gpt_dataset import MultiSplitGPTDataset, MultiSplitGPTDatasetConfig
+# from .query.retro_dataset import get_retro_datasets
