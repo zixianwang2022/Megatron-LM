6c9fb7b552c1abc912f2cb47f8b9db54c48e304e
Lawrence McAfee <lmcafee@nvidia.com>
2024-03-11 11:52:42 -0700

    added values for cross attention masks.

--- megatron/core/models/retro/model/encoder_attention.py
+++ megatron/core/models/retro/model/encoder_attention.py
@@ -81,11 +81,34 @@ class RetroEncoderCrossAttention(BaseRetroCrossAttention):
             # - attention_output: [ r, bs*l, d ]
             # - attention_bias:   [ d ]
             chunked_output = chunked_outputs[:, :, k].contiguous()
+            # >>>
+            chunked_output_mask = torch.full(
+                # size=(1, 1, chunked_output.shape[-1], chunked_output.shape[-1]),
+                # size=(chunked_output.shape[-1], chunked_output.shape[-1]),
+                # size=(chunked_output.shape[-1], 1, 1, chunked_output.shape[-1]),
+                # size=(chunked_output.shape[0], 1, 1, chunked_output.shape[2]),
+                size=(chunked_output.shape[1], 1, 1, chunked_output.shape[0]),
+                fill_value=True,
+                dtype=torch.bool,
+                device=chunked_output.device)
+            # <<<
+            # >>>
+            from lutil import pax
+            pax("chunked_outputs, chunked_output, chunked_output_mask")
+            # <<<
             attention_output, attention_bias = self.attn(
                 hidden_states=chunked_output,  # Q (neighbor embedding)
-                attention_mask=None,
+                # >>>
+                # attention_mask=None,
+                attention_mask=chunked_output_mask,
+                # <<<
                 key_value_states=key_value_states,  # K, V (hidden act)
             )
+
+            # >>>
+            # from lutil import pax
+            # pax("chunked_output, chunked_output_mask, attention_output")
+            # <<<
 
             # Residual connection. [ r, bs*l, d ]
             residual = chunked_output
