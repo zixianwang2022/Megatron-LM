1f7d15ab360a49eb1faf9691254b0c3d4b876069
Roger Waleffe <rwaleffe@nvidia.com>
2024-04-04 15:17:11 -0700

    fix file not found errors when using triton with distributed training

--- megatron/core/ssm/triton_cache_manager.py
+++ megatron/core/ssm/triton_cache_manager.py
@@ -1,9 +1,11 @@
 import torch
 import os
 import random
-
 import json
+
 from time import sleep
+from pathlib import Path
+from typing import Dict, Optional
 
 from triton.runtime.cache import FileCacheManager
 
@@ -12,45 +14,18 @@ def get_rank():
     return torch.distributed.get_rank()
 
 
+def default_cache_dir():
+    return os.path.join(Path.home(), ".triton", "cache")
+
+
 class ParallelFileCacheManager(FileCacheManager):
-
-    def put(self, data, filename, binary=True) -> str:
-        if not self.cache_dir:
-            raise RuntimeError("Could not create or locate cache dir")
-        binary = isinstance(data, bytes)
-        if not binary:
-            data = str(data)
-        assert self.lock_path is not None
-        filepath = self._make_path(filename)
-        # Random ID to avoid any collisions
-        rnd_id = random.randint(0, 1000000)
-        # we use the PID in case a bunch of these around so we can see what PID made it
-        pid = os.getpid()
-
-        # Note (rwaleffe): this barrier prevents one rank from falling behind and noticing the filepath before entering
-        # this function (causing a hang on the second barrier due to not entering this function at all)
-        # Initial dist barrier
-        # torch.distributed.barrier()
-
-        # Write only on rank 0. This prevents FileNotFound errors while compiling triton kernels in parallel
-        # (e.g., distributed training).
-        # if get_rank() == 0:
-
-        # use tempfile to be robust against program interruptions
-        temp_path = f"{filepath}.tmp.pid_{pid}_{rnd_id}"
-        mode = "wb" if binary else "w"
-        with open(temp_path, mode) as f:
-            f.write(data)
-        # Replace is guaranteed to be atomic on POSIX systems if it succeeds
-        # so filepath cannot see a partial write
-        # Note (rwaleffe): try except needed on distributed file systems
-        try:
-            os.replace(temp_path, filepath)
-        except:
-            pass
-            # raise FileNotFoundError(f"FILE NOT FOUND")
-
-        # Final dist barrier
-        # torch.distributed.barrier()
-
-        return filepath
+    def __init__(self, key):
+        self.key = key
+        self.lock_path = None
+        # create cache directory if it doesn't exist
+        self.cache_dir = os.environ.get('TRITON_CACHE_DIR', default_cache_dir())
+        self.cache_dir = os.path.join(self.cache_dir, "rank_{}".format(get_rank()))
+        if self.cache_dir:
+            self.cache_dir = os.path.join(self.cache_dir, self.key)
+            self.lock_path = os.path.join(self.cache_dir, "lock")
+            os.makedirs(self.cache_dir, exist_ok=True)
