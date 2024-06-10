7a03e803c47fe29a08222beb6ba856e611099ede
Duncan Riach <duncan@nvidia.com>
2024-06-10 12:11:21 -0700

    Improve example mamba train script

--- examples/mamba/train.sh
+++ examples/mamba/train.sh
@@ -2,6 +2,29 @@
 
 # Use: train.sh <data-path> <tokenizer-path>
 
+MODEL_SCALE="800M" # or "8B"
+
+case "${MODEL_SCALE}" in
+    "800M")
+        TENSOR_MODEL_PARALLEL_SIZE=1
+        NUM_LAYERS=48
+        HIDDEN_SIZE=1024
+        NUM_ATTENTION_HEADS=16
+        GLOBAL_BATCH_SIZE=32
+        ;;
+    "8B")
+        TENSOR_MODEL_PARALLEL_SIZE=4
+        NUM_LAYERS=56
+        HIDDEN_SIZE=4096
+        NUM_ATTENTION_HEADS=32
+        GLOBAL_BATCH_SIZE=8
+        ;;
+    *)
+        echo "Invalid version specified"
+        exit 1
+        ;;
+esac
+
 DATA_PATH=$1
 TOKENIZER_PATH=$2
 
@@ -27,24 +50,22 @@ LR_WARMUP_SAMPLES=50000
 LR_DECAY_SAMPLES=73192188 # TRAIN_SAMPLES - LR_WARMUP_SAMPLES
 
 options=" \
-       --tensor-model-parallel-size 1 \
+       --tensor-model-parallel-size ${TENSOR_MODEL_PARALLEL_SIZE} \
        --sequence-parallel \
        --pipeline-model-parallel-size 1 \
        --use-distributed-optimizer \
        --overlap-param-gather \
        --overlap-grad-reduce \
-\
        --untie-embeddings-and-output-weights \
        --init-method-std 0.02 \
        --position-embedding-type none \
-       --num-layers 48 \
-       --hidden-size 1024 \
-       --num-attention-heads 16 \
+       --num-layers ${NUM_LAYERS} \
+       --hidden-size ${HIDDEN_SIZE} \
+       --num-attention-heads ${NUM_ATTENTION_HEADS} \
        --group-query-attention \
        --num-query-groups 8 \
        --hybrid-attention-ratio 0.08 \
        --hybrid-mlp-ratio 0.5 \
-       --hybrid-override-pattern M-M-M--M-*M-M-M-M--*M-M-M-M-*M--M-M-M-*M-M--M-M- \
        --seq-length ${SEQ_LEN} \
        --max-position-embeddings ${SEQ_LEN} \
        --train-samples ${TRAIN_SAMPLES} \
@@ -59,7 +80,7 @@ options=" \
        --tokenizer-model ${TOKENIZER_PATH} \
        --distributed-backend nccl \
        --micro-batch-size 4 \
-       --global-batch-size 32 \
+       --global-batch-size ${GLOBAL_BATCH_SIZE} \
        --lr 2.5e-4 \
        --min-lr 2.5e-5 \
        --lr-decay-style cosine \
@@ -71,7 +92,7 @@ options=" \
        --normalization RMSNorm \
        --adam-beta1 0.9 \
        --adam-beta2 0.95 \
-       --log-interval 100 \
+       --log-interval 10 \
        --save-interval 2000 \
        --eval-interval 2000 \
        --eval-iters 32 \
@@ -81,4 +102,4 @@ options=" \
        --no-create-attention-mask-in-dataloader \
        --tensorboard-dir ${TENSORBOARD_DIR}"
 
-torchrun --nproc_per_node 8 ../../pretrain_mamba.py ${options}
+torchrun --nproc_per_node 4 ../../pretrain_mamba.py ${options}
